5334ff06c8b6b1904c438e1e1590051eb99ff1da
Georgi Tsanev <cologneto@gmail.com>
2021-02-05 12:36:04 +0200

    Account and Customer components added 2

---
+++ src/app/customer/customer.component.ts
@@ -0,0 +1,17 @@
+import {Component, Input, OnInit} from '@angular/core';
+import Customer from '../models/customer.model';
+
+@Component({
+  selector: 'app-customer',
+  templateUrl: './customer.component.html',
+  styleUrls: ['./customer.component.css']
+})
+export class CustomerComponent implements OnInit {
+
+  constructor() { }
+
+  @Input() customer: Customer;
+  ngOnInit(): void {
+  }
+
+}
